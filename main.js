299fa09d57c196083840dbcbbffe5b10a8cf632e
Luke Stevens <lcm.stevens96@gmail.com>
2018-09-14 14:03:38 +0100

    Adds necessary change to build electron app

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let window;
+
+function createWindow () {
+  // Create the browser window.
+  window = new BrowserWindow({
+    width: 600, 
+    height: 600,
+    backgroundColor: '#ffffff'
+  })
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname,'/dist/vintagecomputing/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  //// uncomment below to open the DevTools.
+  //window.webContents.openDevTools()
+
+  // Event when the window is closed.
+  window.on('closed', function () {
+    win = null
+  })
+}
+
+// Create window on electron intialization
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
