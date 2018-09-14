afd256c21893b97f20027488b5623a6537da347d
Luke Stevens <lcm.stevens96@gmail.com>
2018-09-14 15:35:48 +0100

    Created an initial leaderboard component

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../team.service';
+import { Score } from '../team';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.css']
 })
+
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+  scores: Score[];
+
+  constructor(private teamService: TeamService) { }
 
   ngOnInit() {
+    this.getAllScores();
+  }
+
+  getAllScores(): void{
+    this.teamService.getTeams().subscribe(teamNames =>{
+      let scores = [];
+      teamNames.forEach(teamName => {
+        this.teamService.getTeam(teamName).subscribe(team => {
+          team.scores.forEach(score => scores.push(score));
+        });
+      });
+      this.scores = scores;
+    });
   }
 
 }
+
